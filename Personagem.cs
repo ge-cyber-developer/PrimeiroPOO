b20dd8a9e6015cd33074b4a39db9f4c92094d5a5
ge-cyber-developer <61585687+ge-cyber-developer@users.noreply.github.com>
2020-07-08 13:32:25 -0300

    first commit

---
+++ Personagem.cs
@@ -0,0 +1,18 @@
+namespace TREINO
+{
+    public class Personagem
+    {
+        
+        public string nome = " Tony Stark";
+
+        public int idade = 35;
+
+        public string armadura;
+
+        public string atacar()
+        {
+
+            return "Personagem atacou";
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace TREINO
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Personagem tony = new Personagem();
+            tony.armadura = "Bleeding Edge";
+
+            Console.WriteLine(tony.nome);
+
+            Console.WriteLine( tony.armadura);
+
+            Console.WriteLine( tony.atacar());
+
+        }
+    }
+}
